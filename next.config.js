17594fde28eae42131737f3998948e5215d61ef5
danyel-ii <222602034+danyel-ii@users.noreply.github.com>
2026-01-07 22:19:49 +0100

    Externalize onnxruntime for helpdesk embeddings

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const nextConfig = {
   basePath,
   assetPrefix: basePath,
   trailingSlash: true,
+  serverExternalPackages: ["@xenova/transformers", "onnxruntime-node"],
+  webpack(config, { isServer }) {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push("onnxruntime-node");
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
